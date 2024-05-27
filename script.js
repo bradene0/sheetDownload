ac969f56a8293c8dfb5c6916a9292101e470d4b4
Braden Evans <bradene0@outlook.com>
2024-05-27 13:08:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,3 @@ var destination = DriveApp.getFolderById("15zslK7yZx5sUL2cL1Xjhe7cU53rAYngl");
 var file = DriveApp.getFileById(SpreadsheetApp.getActiveSpreadsheet().getId())
 file.makeCopy(name, destination);
 }
-//Currently not working
